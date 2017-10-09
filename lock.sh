52e7a6e42b95f9c9711ceed527e2d4a78e04f97c
Matthieu ANTOINE <matthieuinspain@gmail.com>
2017-10-09 21:32:46 +0200

    Forgot to uncomment rm

--- lock.sh
+++ lock.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# i3lock blurred screen inspired by /u/patopop007 and the blog post
+# swaylock blurred screen inspired by /u/patopop007 and the blog post
 # http://plankenau.com/blog/post-10/gaussianlock
-
-# Timings are on an Intel i7-2630QM @ 2.00GHz
+# Modified from gist https://gist.github.com/csivanich/10914698
 
 # Dependencies:
 # imagemagick
-# i3lock
-# scrot (optional but default)
+# swaylock
+# swaygrab
 
 IMAGE=/tmp/swaylock.png
-SCREENSHOT="swaygrab $IMAGE" # 0.46s
+SCREENSHOT="swaygrab $IMAGE"
 
 # Alternate screenshot method with imagemagick. NOTE: it is much slower
 # SCREENSHOT="import -window root $IMAGE" # 1.35s
@@ -20,14 +19,14 @@ SCREENSHOT="swaygrab $IMAGE" # 0.46s
 # Uncomment one to use, if you have multiple, the last one will be used
 
 # All options are here: http://www.imagemagick.org/Usage/blur/#blur_args
-#BLURTYPE="0x5" # 7.52s
-#BLURTYPE="0x2" # 4.39s
-#BLURTYPE="5x2" # 3.80s
-#BLURTYPE="2x8" # 2.90s
-#BLURTYPE="2x3" # 2.92s
+#BLURTYPE="0x5"
+#BLURTYPE="0x2"
+#BLURTYPE="5x2"
+#BLURTYPE="2x8"
+#BLURTYPE="2x3"
 BLURTYPE="0x6"
 # Get the screenshot, add the blur and lock the screen with it
 $SCREENSHOT
 convert $IMAGE -blur $BLURTYPE $IMAGE
 swaylock -i $IMAGE
-#rm $IMAGE
+rm $IMAGE
